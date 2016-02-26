81eb926eec55be0d5b2e2674b2b8d00ffa6036a6
Unicorn IT <unicornit@Unicorns-Mini.fritz.box>
2016-02-26 10:47:01 +0100

    initial commit

--- MyFrameworkTest/ViewController.swift
+++ MyFrameworkTest/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import MyFramework
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginView: MyLoginView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.loginView.loginHandler = self.login
     }
 
+    func login(username: String, password: String) {
+        
+        let alert = UIAlertController(title: "Login", message:"I guess i should log in now", preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default) { _ in })
+        
+        self.presentViewController(alert, animated: true){}
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
